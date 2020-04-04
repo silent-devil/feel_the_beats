a45cc2addad01484d1089d0d9510ebf31c49a143
silent-devil <56071047+silent-devil@users.noreply.github.com>
2020-04-04 05:56:33 +0530

    Add files via upload

---
+++ drum_beats/index.js
@@ -0,0 +1,68 @@
+for (var i = 0; i < document.querySelectorAll(".drum").length; i++) {
+    document.querySelectorAll(".drum")[i].addEventListener("click", function() {
+            var cl = this.innerHTML;
+            sound(cl);
+            buttonanima(cl)
+
+        }
+
+    );
+}
+document.addEventListener("keypress", function(but) {
+    sound(but.key);
+    buttonanima(but.key);
+});
+
+function sound(key) {
+    switch (key) {
+        case "w":
+            {
+                var audio = new Audio("sounds/tom-1.mp3");
+                audio.play();
+                break;
+            }
+        case "a":
+            {
+                var audio = new Audio("sounds/tom-2.mp3");
+                audio.play();
+                break;
+            }
+        case "s":
+            {
+                var audio = new Audio("sounds/tom-3.mp3");
+                audio.play();
+                break;
+            }
+        case "d":
+            {
+                var audio = new Audio("sounds/tom-4.mp3");
+                audio.play();
+                break;
+            }
+        case "j":
+            {
+                var audio = new Audio("sounds/snare.mp3");
+                audio.play();
+                break;
+            }
+        case "k":
+            {
+                var audio = new Audio("sounds/crash.mp3");
+                audio.play();
+                break;
+            }
+        case "l":
+            {
+                var audio = new Audio("sounds/kick.mp3");
+                audio.play();
+                break;
+            }
+    }
+}
+
+function buttonanima(currkey) {
+    document.querySelector("." + currkey).classList.toggle("pressed");
+    setTimeout(function() {
+        document.querySelector("." + currkey).classList.toggle("pressed");
+    }, 100);
+}
